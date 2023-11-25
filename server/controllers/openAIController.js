33ddec66fb279dad4e88c68f9256f2cb69a1bcd0
Oneharry <harryofficialchidi@yahoo.com>
2023-11-25 22:39:33 +0100

    changed http method

--- server/controllers/openAIController.js
+++ server/controllers/openAIController.js
@@ -1,6 +1,9 @@
 const { OpenAI } = require('openai');
 const dotenv = require('dotenv').config();
 
+export const config = {
+   runtime: 'edge', regions: 'iad1'
+}
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
@@ -18,8 +21,11 @@ const prompt = async (content) => {
             {"role": "user", "content": "What medical condition does these symptoms suggest"},
             {"role": "user", "content": content}],
         model: "gpt-3.5-turbo",
+        stream: true
     });
-    return (completion.choices[0].message.content);
+    for await (const chunk of completion) {
+        return chunk
+    }
 }
 
 
@@ -31,9 +37,16 @@ const prompt = async (content) => {
 const getPromptResp = async (req, res) => {
     try {
         const { content } = req.body
-        console.log("CONTENT", content);
-        const completion = await prompt(content);
-        res.json(completion);   
+        // console.log("CONTENT", content);
+        // const completion = await prompt(content);
+        const completion = await openai.chat.completions.create({
+            messages: [{"role": "system", "content": "You are a healthcare provider and symptom diagnoser."},
+                {"role": "user", "content": "What medical condition does these symptoms suggest"},
+                {"role": "user", "content": content}],
+            model: "gpt-3.5-turbo",
+            // stream: true
+        });
+        res.json(completion.choices[0]);   
     } catch (error) {
         console.log("error", error);
         res.json(error);
