f71c4d446aa4ea2a6280146cb11e2363238b4466
Oneharry <harryofficialchidi@yahoo.com>
2023-11-25 23:51:03 +0100

    changed port for cyclic app

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const chatRouter = require('./routes/openAIRoute')
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const app = express()
 
 app.use(cors({
