a4974ef5b3599705fb158936517cf4e6350e51c1
Oneharry <harryofficialchidi@yahoo.com>
2023-11-25 23:55:52 +0100

    changed port for cyclic app

--- server/controllers/openAIController.js
+++ server/controllers/openAIController.js
@@ -1,24 +1,11 @@
 const { OpenAI } = require('openai');
 const dotenv = require('dotenv').config();
 
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
 
-/**
- * @desc handle the api call to openAI
- * @args - content: the prompt
- * @return the response to prompt
- */
-const prompt = async (content) => {
-    const completion = await openai.chat.completions.create({
-        messages: [{"role": "system", "content": "You are a healthcare provider and symptom diagnoser."},
-            {"role": "user", "content": "What medical condition does these symptoms suggest"},
-            {"role": "user", "content": content}],
-        model: "gpt-3.5-turbo",
-        stream: true
-    });
-    for await (const chunk of completion) {
-        return chunk
-    }
-}
+
 
 
 /**
@@ -29,8 +16,6 @@ const prompt = async (content) => {
 const getPromptResp = async (req, res) => {
     try {
         const { content } = req.body
-        // console.log("CONTENT", content);
-        // const completion = await prompt(content);
         const completion = await openai.chat.completions.create({
             messages: [{"role": "system", "content": "You are a healthcare provider and symptom diagnoser."},
                 {"role": "user", "content": "What medical condition does these symptoms suggest"},
