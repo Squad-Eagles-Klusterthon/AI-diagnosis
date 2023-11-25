de5b7de8608234495578ccf481a2a86aac9131a1
Oneharry <harryofficialchidi@yahoo.com>
2023-11-26 00:23:38 +0100

    fixing console error

--- server/controllers/openAIController.js
+++ server/controllers/openAIController.js
@@ -5,6 +5,8 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
 
+
+
 /**
  * @desc Get result from openAI
  * @route GET /api/chats
@@ -19,7 +21,6 @@ const getPromptResp = async (req, res) => {
                 {"role": "user", "content": "What medical condition does these symptoms suggest"},
                 {"role": "user", "content": content}],
             model: "gpt-3.5-turbo",
-            // stream: true
         });
         res.json(completion.choices[0]);   
     } catch (error) {
